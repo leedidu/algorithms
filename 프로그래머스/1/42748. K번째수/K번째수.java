bca8b8c0f3a4db7c24196095164a18890ed5ddf6
Jisoo Lee <99534272+leedidu@users.noreply.github.com>
2024-10-28 18:06:43 +0900

    [level 1] Title: K번째수, Time: 0.04 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,42 +1,6 @@
-import java.util.Arrays;
 class Solution {
-    public static int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-
-        for(int i = 0; i < commands.length; i++) {
-            int[] arr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
-            quickSort(arr, 0, arr.length - 1);
-            answer[i] = arr[commands[i][2] - 1];
-        }
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
         return answer;
     }
-
-    public static void quickSort(int[] arr, int start, int end) {
-        if(start >= end) return;
-        int pivot = partition(arr, start, end);
-        quickSort(arr, start, pivot - 1);
-        quickSort(arr, pivot + 1, end);
-    }
-
-    // 배열을 피벗 기준으로 분할 / 피벗의 최종 위치 반환하는 메서드
-    public static int partition(int[] arr, int start, int end) {
-        int pivot = arr[end];
-        int i = start - 1;
-
-        for (int j = start; j < end; j++) {
-            if(arr[j] <= pivot) {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[end];
-        arr[end] = temp;
-
-        return i + 1;
-    }
-
 }
