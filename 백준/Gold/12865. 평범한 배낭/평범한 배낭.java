e11554d7a445a30a8194d7c2f8db3e2b4183b692
Jisoo Lee <99534272+leedidu@users.noreply.github.com>
2025-08-08 01:29:56 +0900

    [Gold V] Title: 평범한 배낭, Time: 196 ms, Memory: 53996 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,30 +1,44 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        
-        int[] weights = new int[n];
-        int[] values = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            weights[i] = sc.nextInt();
-            values[i] = sc.nextInt();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); // 물품 수
+        int k = Integer.parseInt(st.nextToken()); // 가능한 무게
+
+        int[][] things = new int[n + 1][2];
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            things[i][0] = Integer.parseInt(st.nextToken()); // 무게
+            things[i][1] = Integer.parseInt(st.nextToken()); // 가치
         }
-        
-        int[] dp = new int[k + 1];
-        
-        for (int i = 0; i < n; i++) {
-            for (int w = k; w >= weights[i]; w--) {
-                dp[w] = Math.max(dp[w], dp[w - weights[i]] + values[i]);
+
+        int[][] backpack = new int[n + 1][k + 1];
+
+        for (int i = 1; i <= n; i++) { // 물건
+            for (int j = 1; j <= k; j++) { // 무게
+                if (things[i][0] > j) { // 현재 가능한 무게를 초과한 경우
+                    backpack[i][j] = backpack[i - 1][j];
+                } else {
+                    backpack[i][j] = Math.max(backpack[i - 1][j], backpack[i - 1][j - things[i][0]] + things[i][1]);
+                }
             }
         }
-        
-        System.out.println(dp[k]);
-        
-        sc.close();
+        int[] total = backpack[n];
+        int ans = 0;
+        for (int i = 0; i < total.length; i++) {
+            if(ans < total[i]){
+                ans = total[i];
+            }
+        }
+        System.out.println(ans);
     }
 }
