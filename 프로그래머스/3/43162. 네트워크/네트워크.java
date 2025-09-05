c40dfa92ce226fe85c7a6bd018fa9675769769d4
Jisoo Lee <99534272+leedidu@users.noreply.github.com>
2025-09-05 21:36:26 +0900

    [level 3] Title: 네트워크, Time: 0.28 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,31 @@
-class Solution {
-    public static int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] visited = new boolean[n];
+import java.util.*;
 
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                dfs(computers, i, visited);
+class Solution {
+    static boolean[] visited;
+    static int answer;
+    public int solution(int n, int[][] computers) {
+        answer = 0;
+        visited = new boolean[n];
+        for(int i = 0; i < n; i++){
+            if (!visited[i]) {
+                dfs(i, n, computers);
                 answer++;
             }
         }
         return answer;
     }
-
-    public static boolean[] dfs(int[][] computers, int j, boolean[] visited) {
-        visited[j] = true; // 방문한걸로 수정
-
-        for (int i = 0; i < computers.length; i++) {
-            if (!visited[i] && i != j && computers[j][i] == 1) { // 자기 자신이 아니어야함 + 1로 연결되어있어야함
-                visited = dfs(computers, i, visited); //재귀함수
+    
+    private void dfs(int index, int n, int[][] computers){
+        if(visited[index]){
+            return;
+        }
+        
+        visited[index] = true;
+        
+        for(int i = 0; i < n; i++){
+            if(computers[index][i] == 1){
+                dfs(i, n, computers);
             }
         }
-        return visited;
     }
 }
