d6ef9af985b89d0df1aa1ab00733b2f261f87fa6
Jisoo Lee <99534272+leedidu@users.noreply.github.com>
2025-09-24 21:28:55 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 4.56 ms, Memory: 93.1 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,66 +1,59 @@
 import java.util.*;
+
 class Solution {
-    static int[] dx = {-1, 1, 0, 0}; // 상 하 좌 우 (x축 이동)
-    static int[] dy = {0, 0, -1, 1}; // y축 이동
-    static int[][][] cnt; // x, y, dir 저장
-
-    public static int solution(int[][] board) {
-        int answer = Integer.MAX_VALUE;
-        // 1 = 벽, 0 = 비워진 칸
-        // 출발점 : 0,0 도착점 : n-1,n-1
-        // 직선도로 = 100, 코너(직선도로가 직각으로 만나는 지점) = 500
-        // 최단 경로를 찾아야함
-        cnt = new int[board.length][board.length][4];
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board.length; j++) {
-                for(int k = 0; k < 4; k++) {
-                    cnt[i][j][k] = Integer.MAX_VALUE;
-                }
-            }
-        }
-        bfs(0, 0, -1, board);
-
-        for (int i = 0; i < 4; i++) {
-            answer = Math.min(answer, cnt[board.length - 1][board.length - 1][i]); // 최종 목적지에서의 최소 비용을 찾음
-        }
+    public int solution(int[][] board) {
+        int answer = 0;
+        // 도착지 : n-1, n-1 
+        // 직선도로 : 100, 코너 : 500
+        int[] ans = dijkstra(board.length, board);
+        Arrays.sort(ans);
+        answer = ans[0];
         return answer;
     }
-
-    public static void bfs(int x, int y, int dir, int[][] board){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x, y, dir, 0}); // 위치와 방향 추가
-
-        while(!q.isEmpty()){
-            int[] p = q.poll();
-            int pX = p[0];
-            int pY = p[1];
-            int pDir = p[2];
-            int pCost = p[3];
-
-
-            for(int i = 0; i < 4; i++){
-                int newX = pX + dx[i];
-                int newY = pY + dy[i];
-                int newDir = i;
-                int newCost = pCost;
-
-                if(newX < 0 || newY < 0 || newX >= board.length || newY >= board.length || board[newX][newY] == 1){
-                    continue;
-                }
-                if(pDir == -1) newCost += 100;
-                else{
-                    if(pDir == newDir){
-                        newCost += 100;
-                    } else{
-                        newCost += 600;
+    
+    public int[] dijkstra(int n, int[][] board){
+        int[][][] dist = new int[n][n][4];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Arrays.fill(dist[i][j], Integer.MAX_VALUE);
+            }
+        }
+        dist[0][0][0] = -1;
+        dist[0][0][1] = -1;
+        dist[0][0][2] = -1;
+        dist[0][0][3] = -1;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[3] - b[3]);
+        pq.offer(new int[]{0, 0, -1, 0});
+        
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
+        
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                int ndir = i;
+                if(nx < n && ny < n && nx >= 0 && ny >= 0){ // 범위 내
+                    if(board[nx][ny] == 0){ // 갈 수 있는 곳
+                        int newCost;
+                        if (cur[2] == -1) {
+                            newCost = cur[3] + 100;
+                        } else if (cur[2] == ndir) {
+                            newCost = cur[3] + 100;
+                        } else { // 코너
+                            newCost = cur[3] + 600;
+                        }
+                        if(dist[nx][ny][i] > newCost){
+                            dist[nx][ny][i] = newCost;
+                            pq.offer(new int[]{nx, ny, i, newCost});
+                        }
                     }
-                }
-
-                if(cnt[newX][newY][newDir] > newCost){ // 더 적은 가격으로 갱신가능
-                    cnt[newX][newY][newDir] = newCost;
-                    q.add(new int[]{newX, newY, newDir, newCost});
                 }
             }
         }
+        return dist[n - 1][n - 1];
     }
 }
