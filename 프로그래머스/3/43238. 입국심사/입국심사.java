2f55790572312ca4087c907566d8aca83ae7e044
Jisoo Lee <99534272+leedidu@users.noreply.github.com>
2025-09-25 00:14:06 +0900

    [level 3] Title: 입국심사, Time: 159.79 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,29 +1,32 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public static long solution(int n, int[] times) {
+    public long solution(int n, int[] times) {
         long answer = 0;
-        // n = 입국심사를 기다리는 사람 수, times = 각 심사관이 한명을 심사하는데 걸리는 시간
-        Arrays.sort(times);
-        long start = 1;
-        long end = (long) times[times.length - 1] * n; // 최악의 경우
-        // 비어있는 심사대보다 비지않은 곳에서 받는 것이 더빠르다면 거기로 가야함
-        while(start <= end) {
-            long mid = (start+end)/2;
-            long sum = 0;
-            // 전체 심사위원의 수 > 인원이면 줄일수있음
-            for(int i : times){
-                sum += mid/i; // 심사관이 mid 시간 동안 처리할 수 있는 사람의 수
-            }
+        
+        int[] t = new int[times.length];
+        for(int i = 0; i < times.length; i++){
+            t[i] = times[i];
+        }
+        Arrays.sort(t);
+        long left = 1;
+        long right = (long)t[t.length - 1] * n;
 
-            if(sum >= n) { // mid시간 동안 모두 심사 가능
+        while(true){
+            if(left > right) break;
+            
+            long mid = (left + right) / 2;
+            long num = 0;
+            for(int i = 0; i < times.length; i++){
+                num += (mid/times[i]);
+            }
+            if(num >= n){
                 answer = mid;
-                end = mid - 1; // 더 짧은 시간에서 탐색
-            } else {
-                start = mid + 1; // mid가 짧다는 말 -> 더 긴시간에서 탐색
+                right = mid - 1;
+            } else{
+                left = mid + 1;
             }
         }
-
         return answer;
     }
 }
