2ce268b65e0a3d506a235f09282180c2dec44abd
Jisoo Lee <99534272+leedidu@users.noreply.github.com>
2025-09-30 02:10:50 +0900

    [level 3] Title: 가장 먼 노드, Time: 26.50 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,48 +1,58 @@
 import java.util.*;
-class Solution {
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    static boolean[] visited;
 
-    public static int solution(int n, int[][] edge) {
-        visited = new boolean[n + 1];
-
-        for(int i = 0; i <= n; i++) {
-            graph.add(i, new ArrayList<>());
-        }
-
-        for(int i = 0; i < edge.length; i++) {
-            graph.get(edge[i][0]).add(edge[i][1]);
-            graph.get(edge[i][1]).add(edge[i][0]);
-        }
-
-        return bfs(1);
+class Node {
+    int now;
+    ArrayList<Integer> to;
+    
+    Node(int now, ArrayList<Integer> to){
+        this.now = now;
+        this.to = to;
     }
+}
 
-    public static int bfs(int n) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(n);
-        visited[n] = true;
-        int cnt = 0;
-
-        while(true){
-            Queue<Integer> temp = new LinkedList<>();
-
-            while(!queue.isEmpty()){
-                int cur = queue.poll();
-                for(int i : graph.get(cur)){
-                    if(!visited[i]){
-                        temp.add(i);
-                        visited[i] = true;
-                    }
+class Solution {
+    public int solution(int n, int[][] edge) {
+        int answer = 0;
+        Node[] nodes = new Node[n + 1];
+        for (int i = 1; i <= n; i++) {
+            nodes[i] = new Node(i, new ArrayList<>());
+        }
+        
+        for(int[] e: edge){
+            nodes[e[0]].to.add(e[1]);
+            nodes[e[1]].to.add(e[0]);
+        }
+        
+        Queue<Integer> q= new LinkedList();
+        q.offer(1);
+        boolean[]visited = new boolean[n+1];
+        visited[1] = true;
+        int[] dist = new int[n + 1];
+        
+        while(!q.isEmpty()){
+            int now = q.poll();
+            
+            for(int i = 0; i < nodes[now].to.size(); i++){
+                int next = nodes[now].to.get(i);
+                
+                if(!visited[next]){
+                    visited[next] = true;
+                    dist[next] = dist[now] + 1;
+                    q.offer(next);
                 }
             }
-
-            if(temp.isEmpty()){
-                break;
-            }
-            queue.addAll(temp);
-            cnt = temp.size();
         }
-        return cnt;
+        int max = 0;
+        for(int i : dist){
+            if(max < i){
+                max = i;
+            }
+        }
+        for(int i= 0; i < dist.length; i++){
+            if(dist[i] == max){
+                answer++;
+            }
+        }
+        return answer;
     }
 }
